410a9e9c7934a3e145f001611c033ec4dcaa2571
Luis Aaron Blas <71951748+labrdev@users.noreply.github.com>
2022-12-08 19:21:00 -0500

    Streamlit y modulos iniciales

---
+++ despliegue/home.py
@@ -0,0 +1,16 @@
+
+import streamlit as st
+
+def app():
+    st.title('Integrantes:')
+    st.write("Erick Alberto Chávez Silupú")
+    st.write("Jeanpiere Julian Palacios Barrutia")
+    st.write("Luis Aaron Blas Ruiz")
+    st.write("Geiner Campos  Cabanillas ")
+    st.write("Diego Carrion Ramos")
+    st.write("Alexander Gabriel Cuello Apaza")
+    st.write("Nel Tocas Chipana")
+    st.write("Luis Quispe")
+    st.write("Manuel Vallejos")
+    st.write("Angel Giovani Falcon Falconi")
+    st.write("Marco Alejando Sánchez Alvarado")
